fda978440f477568488a9a70cbd2e78233428b7e
firthunsps@gmail.com <firthunsps@gmail.com>
2021-09-22 20:35:05 +0200

    anexado nuestra url de mongoose....

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RestaurantesModule } from './restaurantes/restaurantes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://ferRoot:AUsFh5UvSKmApSG@cluster0.cyira.mongodb.net/restauranteNestjs?retryWrites=true&w=majority'
+    ),
+    RestaurantesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
