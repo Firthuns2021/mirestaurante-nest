4f70604d021d62e2ede5707be523d1318451cde5
firthunsps@gmail.com <firthunsps@gmail.com>
2022-02-05 18:12:58 +0100

    sesion clase dia Febrero  2022

--- src/countries/services/country/country.service.ts
+++ src/countries/services/country/country.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { Country } from "../../interfaces/country.interface";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CountriesStates } from '../../interfaces/country.interface';
 
 @Injectable()
 export class CountryService {
   constructor(
     @InjectModel('Country')
-    private countryModel: Model<Country>,
+    private countryModel: Model<CountriesStates>,
   ) {}
-  async getCountries(): Promise<Country[]> {
+  async getCountries(): Promise<CountriesStates[]> {
     return await this.countryModel.find();
   }
-  async getStates(country: string): Promise<Country> {
+  async getStates(country: string): Promise<CountriesStates> {
     return await this.countryModel.findOne({ country: country});
   }
 }
